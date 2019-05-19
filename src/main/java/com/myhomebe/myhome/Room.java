4ca63a5b059feae948e63085443038a844714d4c
nagerz <znager@gmail.com>
2019-05-19 11:48:20 -0600

    comments out progress code

--- src/main/java/com/myhomebe/myhome/Room.java
+++ src/main/java/com/myhomebe/myhome/Room.java
@@ -1,22 +1,22 @@
-package com.myhomebe.myhome;
-
-import javax.persistence.Id;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Entity;
-import java.util.Objects;
-import javax.persistence.Table;
-
-@Entity
-@Table(name = "rooms")
-
-public class Room {
-    @Id
-    @GeneratedValue
-    @GraphQLQuery(name = "id", description = "A project's id")
-    private Long id;
-    @GraphQLQuery(name = "name", description = "A project's name")
-    private @NonNull String name;
-    @GraphQLQuery(name = "description", description = "A project's description")
-    private String description;
-}
+// package com.myhomebe.myhome;
+//
+// import javax.persistence.Id;
+// import javax.persistence.GeneratedValue;
+// import javax.persistence.GenerationType;
+// import javax.persistence.Entity;
+// import java.util.Objects;
+// import javax.persistence.Table;
+//
+// @Entity
+// @Table(name = "rooms")
+//
+// public class Room {
+//     @Id
+//     @GeneratedValue
+//     @GraphQLQuery(name = "id", description = "A project's id")
+//     private Long id;
+//     @GraphQLQuery(name = "name", description = "A project's name")
+//     private @NonNull String name;
+//     @GraphQLQuery(name = "description", description = "A project's description")
+//     private String description;
+// }
