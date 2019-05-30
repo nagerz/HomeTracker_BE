2be7488222987d2b85387336eb40b49536b72774
nagerz <znager@gmail.com>
2019-05-29 22:09:02 -0600

    Adds update room endpoint test. Adds dirtiescontext to reset db before each test.

--- src/test/java/com/myhomebe/features/GraphQLControllerTest.java
+++ src/test/java/com/myhomebe/features/GraphQLControllerTest.java
@@ -5,6 +5,7 @@ import org.json.simple.JSONObject;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.net.URI;
@@ -20,6 +21,10 @@ import org.springframework.http.ResponseEntity;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment=WebEnvironment.RANDOM_PORT)
+// Syntax would be used for using different database than development
+// @SpringBootTest(webEnvironment=WebEnvironment.RANDOM_PORT, properties = {"spring.config.name=my-home-be-test-h2","my-home-be.trx.datasource.url=jdbc:h2:mem:trxServiceStatus"})
+// Used to reset database before each test
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class GraphQLControllerTest{
   @Autowired
   private TestRestTemplate restTemplate;
@@ -58,7 +63,7 @@ public class GraphQLControllerTest{
 
     ResponseEntity<String> result = this.restTemplate.postForEntity(uri, request, String.class);
 
-    String expected = "{\"data\":{\"projects\":[{\"id\":1,\"name\":\"House 1\",\"description\":\"Big, white house\",\"address\":\"123 Fake St.\",\"city\":\"Denver\",\"state\":\"CO\",\"zip_code\":\"80205\",\"rooms\":[{\"id\":1},{\"id\":2}]},{\"id\":3,\"name\":\"House 4\",\"description\":\"made by graphql\",\"address\":\"another address\",\"city\":null,\"state\":null,\"zip_code\":null,\"rooms\":[{\"id\":4},{\"id\":5}]},{\"id\":2,\"name\":\"Updated House\",\"description\":\"Small with picket fence\",\"address\":\"456 Random Lane\",\"city\":\"Different City\",\"state\":\"CO\",\"zip_code\":\"80205\",\"rooms\":[{\"id\":3}]}]}}";
+    String expected = "{\"data\":{\"projects\":[{\"id\":1,\"name\":\"House 1\",\"description\":\"Big, white house\",\"address\":\"123 Fake St.\",\"city\":\"Denver\",\"state\":\"CO\",\"zip_code\":\"80205\",\"rooms\":[{\"id\":1},{\"id\":2}]},{\"id\":2,\"name\":\"House 2\",\"description\":\"Small with picket fence\",\"address\":\"456 Random Lane\",\"city\":\"Denver\",\"state\":\"CO\",\"zip_code\":\"80205\",\"rooms\":[{\"id\":3}]}]}}";
 
     //Verify request succeed
     Assert.assertEquals(200, result.getStatusCodeValue());
@@ -89,13 +94,13 @@ public class GraphQLControllerTest{
     URI uri = new URI(baseUrl);
 
 		JSONObject body = new JSONObject();
-		body.put("query", "mutation{updateProject(project: {id: 2, name: \"Updated House\", city: \"Different City\"}) {id name city state}}");
+		body.put("query", "mutation{updateProject(project: {id: 2, name: \"Updated House\", description: \"test project update\", address: \"updated address\", city: \"Different City\", state: \"Different state\", zip_code: \"Different zip\"}) {id name description address city state zip_code}}");
 
     HttpEntity<JSONObject> request = new HttpEntity<JSONObject>(body);
 
     ResponseEntity<String> result = this.restTemplate.postForEntity(uri, request, String.class);
 
-    String expected = "{\"data\":{\"updateProject\":{\"id\":2,\"name\":\"Updated House\",\"city\":\"Different City\",\"state\":\"CO\"}}}";
+    String expected = "{\"data\":{\"updateProject\":{\"id\":2,\"name\":\"Updated House\",\"description\":\"test project update\",\"address\":\"updated address\",\"city\":\"Different City\",\"state\":\"Different state\",\"zip_code\":\"Different zip\"}}}";
     //Verify request succeed
     Assert.assertEquals(200, result.getStatusCodeValue());
     Assert.assertEquals(expected, result.getBody());
@@ -160,6 +165,52 @@ public class GraphQLControllerTest{
     ResponseEntity<String> result3 = this.restTemplate.postForEntity(uri, request3, String.class);
 
     String expected3 = "{\"data\":{\"room\":{\"id\":2,\"roomMaterials\":[{\"element_type\":\"Flooring\",\"material\":{\"name\":\"Updated material\",\"vendor\":\"Updated vendor\",\"brand\":\"Updated Brand\",\"model_number\":\"Updated number\",\"manual_url\":\"Updated url\",\"notes\":\"Updated notes\",\"quantity\":5.0,\"unit_price\":500.0}}]}}}";
+
+    Assert.assertEquals(200, result3.getStatusCodeValue());
+    Assert.assertEquals(expected3, result3.getBody());
+  }
+
+  @Test
+  public void testUpdateRoom() throws URISyntaxException{
+    final String baseUrl = "http://localhost:"+randomServerPort+"/api/v1/graphql/";
+    URI uri = new URI(baseUrl);
+
+    //Before update
+		JSONObject body1 = new JSONObject();
+		body1.put("query", "{room(id: 1) {id name type description}}");
+
+    HttpEntity<JSONObject> request1 = new HttpEntity<JSONObject>(body1);
+
+    ResponseEntity<String> result1 = this.restTemplate.postForEntity(uri, request1, String.class);
+
+    String expected1 = "{\"data\":{\"room\":{\"id\":1,\"name\":\"Living Room 1\",\"type\":\"Living Room\",\"description\":\"Northeast living room\"}}}";
+
+    Assert.assertEquals(200, result1.getStatusCodeValue());
+    Assert.assertEquals(expected1, result1.getBody());
+
+    //Update request
+    JSONObject body2 = new JSONObject();
+		body2.put("query", "mutation{updateRoom(room: {id: 1, name: \"Updated Bedroom\", type: \"updated type\", description: \"test update room\"}) {id name type description roomMaterials{id}}}");
+
+    HttpEntity<JSONObject> request2 = new HttpEntity<JSONObject>(body2);
+
+    ResponseEntity<String> result2 = this.restTemplate.postForEntity(uri, request2, String.class);
+
+    String expected2 = "{\"data\":{\"updateRoom\":{\"id\":1,\"name\":\"Updated Bedroom\",\"type\":\"updated type\",\"description\":\"test update room\",\"roomMaterials\":[{\"id\":1},{\"id\":2}]}}}";
+
+    //Verify request succeed
+    Assert.assertEquals(200, result2.getStatusCodeValue());
+    Assert.assertEquals(expected2, result2.getBody());
+
+    //Verify database after request
+    JSONObject body3 = new JSONObject();
+    body3.put("query", "{room(id: 1) {id name type description}}");
+
+    HttpEntity<JSONObject> request3 = new HttpEntity<JSONObject>(body3);
+
+    ResponseEntity<String> result3 = this.restTemplate.postForEntity(uri, request3, String.class);
+
+    String expected3 = "{\"data\":{\"room\":{\"id\":1,\"name\":\"Updated Bedroom\",\"type\":\"updated type\",\"description\":\"test update room\"}}}";
 
     Assert.assertEquals(200, result3.getStatusCodeValue());
     Assert.assertEquals(expected3, result3.getBody());
